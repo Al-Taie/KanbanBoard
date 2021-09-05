4eef3527119fd1642fae8ab0528a82a32c1221f3
Al-Taie <agprosup@gmail.com>
2021-09-05 15:47:26 +0300

    feat: add TaskDbHelper class

--- app/src/main/java/com/watermelon/kanbanboard/ui/MainActivity.kt
+++ app/src/main/java/com/watermelon/kanbanboard/ui/MainActivity.kt
@@ -1,16 +1,36 @@
 package com.watermelon.kanbanboard.ui
 
+import android.content.ContentValues
 import android.view.LayoutInflater
 import com.watermelon.kanbanboard.R
+import com.watermelon.kanbanboard.data.TaskDbHelper
 import com.watermelon.kanbanboard.databinding.ActivityMainBinding
 import com.watermelon.kanbanboard.ui.base.BaseActivity
 
 class MainActivity : BaseActivity<ActivityMainBinding>() {
+    private lateinit var databaseHelper: TaskDbHelper
     override val theme = R.style.Theme_KanbanBoard
 
-    override fun setup() {}
+    override fun setup() {
+        databaseHelper = TaskDbHelper(applicationContext)
+    }
 
-    override fun callBack() {}
+    override fun callBack() {
+        addTask()
+    }
+
+    private fun addTask() {
+        val newEntry = ContentValues()
+
+        with(TaskDbHelper.DB) {
+            newEntry.apply {
+                put(TITLE, "TEST")
+                put(DESCRIPTION, "TEST")
+                put(STATUS, "TEST")
+            }
+            databaseHelper.writableDatabase.insert(TABLE_NAME, null, newEntry)
+        }
+    }
 
     override val inflate: (LayoutInflater) -> ActivityMainBinding
         get() = ActivityMainBinding::inflate

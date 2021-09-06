396a68e27a8abf6d3a622e4de8adb597926bd1d8
Al-Taie <agprosup@gmail.com>
2021-09-06 11:04:39 +0300

    feat: add listener to TodoFragment

--- app/src/main/java/com/watermelon/kanbanboard/ui/todo/TodoFragment.kt
+++ app/src/main/java/com/watermelon/kanbanboard/ui/todo/TodoFragment.kt
@@ -3,10 +3,11 @@ package com.watermelon.kanbanboard.ui.todo
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import com.watermelon.kanbanboard.databinding.FragmentTodoBinding
-import com.watermelon.kanbanboard.ui.adapter.ItemAdapter
+import com.watermelon.kanbanboard.ui.add.AddFragment
 import com.watermelon.kanbanboard.ui.base.BaseFragment
+import com.watermelon.kanbanboard.ui.interfaces.CustomDialogFragment
 
-class TodoFragment : BaseFragment<FragmentTodoBinding>() {
+class TodoFragment(private val listener: CustomDialogFragment) : BaseFragment<FragmentTodoBinding>() {
     override val inflate: (LayoutInflater, ViewGroup?, attachToRoot: Boolean) -> FragmentTodoBinding
         get() = FragmentTodoBinding::inflate
 
@@ -15,5 +16,6 @@ class TodoFragment : BaseFragment<FragmentTodoBinding>() {
     override fun callBack() {
 //        val adapter = ItemAdapter(DataManager.Data)
 //        binding.todoRecyclerview.adapter = adapter
+        binding.addButton.setOnClickListener { listener.showDialog(AddFragment(listener)) }
     }
 }

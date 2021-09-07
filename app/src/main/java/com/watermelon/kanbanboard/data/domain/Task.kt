f626fd74bd4403f4a9ef76215105fdde33c53cbb
Al-Taie <agprosup@gmail.com>
2021-09-07 19:31:38 +0300

    feat: add data manager

--- app/src/main/java/com/watermelon/kanbanboard/data/domain/Task.kt
+++ app/src/main/java/com/watermelon/kanbanboard/data/domain/Task.kt
@@ -1,10 +1,12 @@
 package com.watermelon.kanbanboard.data.domain
 
 data class Task(
+    val id: Int,
     val title: String,
     val description: String,
     val assignedTo: String,
     val dueDate: String,
     val status: String,
+    val tableName: String,
     val expanded: Boolean = false
 )

666790ac593983d0636463b8fcde00a818139b6b
Al-Taie <agprosup@gmail.com>
2021-09-08 23:32:00 +0300

    fix: update data class

--- app/src/main/java/com/watermelon/kanbanboard/data/domain/Task.kt
+++ app/src/main/java/com/watermelon/kanbanboard/data/domain/Task.kt
@@ -1,13 +1,13 @@
 package com.watermelon.kanbanboard.data.domain
 
 data class Task(
-    val id: Int ,
+    val id: Int,
     val title: String,
     val description: String,
     val assignedTo: String,
     val dueDate: String,
     val status: String,
-    val tableName: String,
+    var tableName: String,
     val expanded: Boolean = false
 
 )

229f424a3af4a71f6f554e336b9203f7d6a23e28
Al-Taie <agprosup@gmail.com>
2021-09-08 07:03:13 +0300

    feat: add new extension function

--- app/src/main/java/com/watermelon/kanbanboard/util/Extensions.kt
+++ app/src/main/java/com/watermelon/kanbanboard/util/Extensions.kt
@@ -1,10 +1,25 @@
 package com.watermelon.kanbanboard.util
 
 import com.watermelon.kanbanboard.R
+import com.watermelon.kanbanboard.data.database.TaskDbHelper.TABLES
+import com.watermelon.kanbanboard.data.domain.Task
 
-fun ArrayList<CustomSpinnerItem>.getCustomItemList(): ArrayList<CustomSpinnerItem>{
-    this.add(CustomSpinnerItem("To Do", R.drawable.ic_todo))
-    this.add(CustomSpinnerItem("In Progress", R.drawable.ic_in_progress))
-    this.add(CustomSpinnerItem("Done", R.drawable.ic_done))
+fun ArrayList<CustomSpinnerItem>.initData(): ArrayList<CustomSpinnerItem> {
+    this.add(CustomSpinnerItem(TABLES.TO_DO, "To Do", R.drawable.ic_todo))
+    this.add(CustomSpinnerItem(TABLES.IN_PROGRESS, "In Progress", R.drawable.ic_in_progress))
+    this.add(CustomSpinnerItem(TABLES.DONE, "Done", R.drawable.ic_done))
     return this
+}
+
+/**
+ * @param index Integer or Null
+ * @param task Task Object
+ * @author     Ahmed Mones
+ * @return     Unit
+ * */
+fun MutableList<Task>.add(index: Int?, task: Task) {
+    if (index == null)
+        this.add(task)
+    else
+        this.add(index, task)
 }

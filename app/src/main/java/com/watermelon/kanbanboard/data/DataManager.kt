b519d7e4929557befa0881203e4c07129eb4d021
Al-Taie <agprosup@gmail.com>
2021-09-08 07:03:22 +0300

    fix: update DataManager

--- app/src/main/java/com/watermelon/kanbanboard/data/DataManager.kt
+++ app/src/main/java/com/watermelon/kanbanboard/data/DataManager.kt
@@ -1,15 +1,125 @@
 package com.watermelon.kanbanboard.data
 
+import com.watermelon.kanbanboard.data.database.TaskDbHelper.TABLES
 import com.watermelon.kanbanboard.data.domain.Task
+import com.watermelon.kanbanboard.util.add
 
 object DataManager {
     private val todoTasksList = mutableListOf<Task>()
     private val inProgressTasksList = mutableListOf<Task>()
     private val doneTasksList = mutableListOf<Task>()
 
-    fun addTodoTask(task: Task) = todoTasksList.add(task)
-    fun addInProgressTask(task: Task) = inProgressTasksList.add(task)
-    fun addDoneTask(task: Task) = doneTasksList.add(task)
+    /**
+     * @param task Task Object
+     * @param index Integer or Null
+     * @author     Ahmed Mones
+     * @return     Int
+     * */
+    fun addTodoTask(task: Task, index: Int? = null) = todoTasksList.add(index, task)
+
+    /**
+     * @param task Task Object
+     * @param index Integer or Null
+     * @author     Ahmed Mones
+     * @return     Int
+     * */
+    fun addInProgressTask(task: Task, index: Int? = null) = inProgressTasksList.add(index, task)
+
+    /**
+     * @param task Task Object
+     * @param index Integer or Null
+     * @author     Ahmed Mones
+     * @return     Int
+     * */
+    fun addDoneTask(task: Task, index: Int? = null) = doneTasksList.add(index, task)
+
+    /**
+     * @param task Task Object
+     * @author     Ahmed Mones
+     * @return     Int
+     * */
+    private fun removeTodoTask(task: Task) : Int {
+        val position = todoTasksList.indexOf(task)
+        todoTasksList.remove(task)
+        return position
+    }
+
+    /**
+     * @param task Task Object
+     * @author     Ahmed Mones
+     * @return     Int
+     * */
+    private fun removeProgressTask(task: Task) : Int {
+        val position = inProgressTasksList.indexOf(task)
+        inProgressTasksList.remove(task)
+        return position
+    }
+
+    /**
+     * @param task Task Object
+     * @author     Ahmed Mones
+     * @return     Int
+     * */
+    private fun removeTask(task: Task) : Int {
+        val position = inProgressTasksList.indexOf(task)
+        inProgressTasksList.remove(task)
+        return position
+    }
+
+    /**
+     * @param oldTask Task Object
+     * @param newTask Task Object
+     * @author     Ahmed Mones
+     * @return     Unit
+     * */
+    fun replaceTodoTask(oldTask: Task, newTask: Task) {
+        val index = removeTodoTask(oldTask)
+        todoTasksList.add(index, newTask)
+    }
+
+    /**
+     * @param oldTask Task Object
+     * @param newTask Task Object
+     * @author     Ahmed Mones
+     * @return     Unit
+     * */
+    fun replaceInProgressTask(oldTask: Task, newTask: Task) {
+        val index = removeTodoTask(oldTask)
+        todoTasksList.add(index, newTask)
+    }
+
+    /**
+     * @param oldTask Task Object
+     * @param newTask Task Object
+     * @author     Ahmed Mones
+     * @return     Unit
+     * */
+    fun replaceDoneTask(oldTask: Task, newTask: Task) {
+        val index = removeTodoTask(oldTask)
+        todoTasksList.add(index, newTask)
+    }
+
+    /**
+     * @param task Task Object
+     * @param to   Table Name
+     * @author     Ahmed Mones
+     * @return     Unit
+     * */
+    fun moveTask(task: Task, to: String) {
+        TABLES.apply {
+            val position = when (task.tableName) {
+                TO_DO -> removeTodoTask(task)
+                IN_PROGRESS -> removeProgressTask(task)
+                else -> removeTask(task)
+            }
+
+            when (to) {
+                TO_DO -> addTodoTask(task = task, index = position)
+                IN_PROGRESS -> addInProgressTask(task = task, index = position)
+                DONE -> addDoneTask(task = task, index = position)
+            }
+        }
+    }
 
     val todoList: List<Task>
         get() = todoTasksList.toList()

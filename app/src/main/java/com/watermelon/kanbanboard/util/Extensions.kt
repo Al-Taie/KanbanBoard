355ed6d4f84bcc6df206c9e1a4d37b904ba6679a
Al-Taie <agprosup@gmail.com>
2021-09-09 16:17:55 +0300

    fix: card animation bug

--- app/src/main/java/com/watermelon/kanbanboard/util/Extensions.kt
+++ app/src/main/java/com/watermelon/kanbanboard/util/Extensions.kt
@@ -1,5 +1,11 @@
 package com.watermelon.kanbanboard.util
 
+import android.view.Gravity
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.transition.Slide
+import androidx.transition.TransitionManager
 import com.watermelon.kanbanboard.data.domain.Task
 
 /**
@@ -16,10 +22,11 @@ fun MutableList<Task>.add(index: Int?, task: Task) {
     }
 }
 
+
 /**
- * return last task database id
+ * Get last id from list of tasks
  * @author     Ahmed Mones
- * @return     Int
+ * @return     Unit
  * */
 fun List<Task>.lastId(): Int {
     return try {
@@ -27,4 +34,20 @@ fun List<Task>.lastId(): Int {
     } catch (e: Exception) {
         1
     }
+}
+
+/**
+ * Make slide animation on change view visibility
+ * @author     Ahmed Mones
+ * @see        <a href="https://stackoverflow.com/a/67856091/16375959">Stakoverfolw Answer</a>
+ * @return     Unit
+ * */
+fun View.slideVisibility(visibility: Boolean, duration: Long = 350) {
+    val transition = Slide(Gravity.BOTTOM)
+    transition.apply {
+        this.duration = duration
+        addTarget(this@slideVisibility)
+    }
+    TransitionManager.beginDelayedTransition(parent as ViewGroup, transition)
+    isVisible = visibility
 }
